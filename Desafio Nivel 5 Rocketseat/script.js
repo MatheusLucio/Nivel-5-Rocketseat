1c06bf6782c4a24754524cdbe9da2f24a9a21c5e
Matheus Lucio <108240104+MatheusLucio@users.noreply.github.com>
2024-07-14 20:01:33 -0300

    Add files via upload

---
+++ Desafio Nivel 5 Rocketseat/script.js
@@ -0,0 +1,132 @@
+// Exercicio 1 - alert ('hello World!')
+
+
+
+
+/* Exercicio 2
+let numberOne = prompt('Escolha o seu primeiro número')
+let numberTwo = prompt('Escolha o seu segundo número')
+
+numberOne = Number(numberOne)
+numberTwo = Number(numberTwo)
+
+const somaNumeros = numberOne + numberTwo
+
+alert ('A soma dos dois números é: ' + somaNumeros) */
+
+
+
+
+
+/* Exercicio 3 
+let variavel = prompt('Declare uma variável a sua escolha:')
+
+if (!isNaN(variavel) && variavel.trim() !== "") {
+    variavel = Number(variavel);
+}
+
+if (typeof variavel === 'number'){
+    alert ('É um numero')}
+    
+else if (typeof variavel === 'string'){
+    alert ('É um texto')} */
+
+
+
+
+
+
+/* Exercicio 4
+let variavel = prompt('Declare uma variável a sua escolha:')
+
+if (!isNaN(variavel) && variavel.trim() !== "") {
+    alert('Não é um texto, essa variável é um número)')
+} else {
+    alert('É um texto');} */
+
+
+
+
+
+/* Exercicio 5
+let variavel = prompt('Declare uma variável a sua escolha:')
+
+if (variavel.toLowerCase() === 'true') {
+    variavel = true
+} else if (variavel.toLowerCase() === 'false') {
+    variavel = false
+}
+if (typeof variavel === 'boolean') {
+    alert('É um booleano')
+} else {
+    alert('Não é um booleano')
+}*/
+
+
+
+
+/* Exercicio 6
+let numberOne = prompt('Escolha o seu primeiro número')
+let numberTwo = prompt('Escolha o seu segundo número')
+
+numberOne = Number(numberOne)
+numberTwo = Number(numberTwo)
+
+const somaNumeros = numberOne - numberTwo */
+
+
+
+
+
+
+/* Exercicio 7
+let numberOne = prompt('Escolha o seu primeiro número')
+let numberTwo = prompt('Escolha o seu segundo número')
+
+numberOne = Number(numberOne)
+numberTwo = Number(numberTwo)
+
+const somaNumeros = numberOne * numberTwo */
+
+
+
+
+
+
+/* Exercicio 8
+let numberOne = prompt('Escolha o seu primeiro número')
+let numberTwo = prompt('Escolha o seu segundo número')
+
+numberOne = Number(numberOne)
+numberTwo = Number(numberTwo)
+
+const somaNumeros = numberOne / numberTwo */
+
+
+
+
+
+/* Exercicio 9
+let variavel = prompt('Declare um número a sua escolha:')
+
+variavel = Number(variavel)
+
+if (!isNaN(variavel) && variavel % 2 === 0) {
+    alert('É um número par')
+} else {
+    alert('Não é um número par')
+}*/
+
+
+
+
+/* Exercicio 10
+let variavel = prompt('Declare um número a sua escolha:')
+
+variavel = Number(variavel);
+
+if (!isNaN(variavel) && variavel % 2 !== 0) {
+    alert('É um número ímpar')
+} else {
+    alert('Não é um número ímpar')
+} */
